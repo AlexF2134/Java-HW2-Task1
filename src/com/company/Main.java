3b67b62688b80152812da80302ed8fa21a05d812
AlexF2134 <alexeyf1@yandex.ru>
2022-02-17 23:35:58 +0500

    " Initial commit "

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int ticketPrice = 2_000;
+        int milePrice = 20;
+
+        int bonus = (ticketPrice / milePrice);
+        System.out.println(bonus);
+    }
+}
